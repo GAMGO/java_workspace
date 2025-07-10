f69e0f416b7781461841aa4e36ca99ac519bc5db
GAMGO <gamgo445@gmail.com>
2025-07-10 09:33:34 +0900

    update 2

--- PARTA/src/App.java
+++ PARTA/src/App.java
@@ -1,7 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         /*App 클래스에서 정의 된 메소드 : main 메소드는 프로그램 실행을 해주는 메소드*/
         System.out.println("Hello, World!");
-        
+        int age = 29;
+        char gender = 'm';
+        String name = "Min";
+        boolean isOk = true;
+        System.out.println("name"+name);
+        System.out.println("bool"+(isOk?"성인":"미성년"));
+        System.out.println("char"+gender);
+        System.out.println("age?"+age);
     }
 }
