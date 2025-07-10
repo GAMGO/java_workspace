f2dc41d3e1eec231787ba74aac54a816fca9a9b4
GAMGO <gamgo445@gmail.com>
2025-07-10 11:26:05 +0900

    update 4

--- PARTA/src/day02/ObejctTest.java
+++ PARTA/src/day02/ObejctTest.java
@@ -13,7 +13,17 @@ public class ObejctTest {
         momo.Math=89;
         momo.Eng=88;
         System.out.println("정보"+momo);
-        System.out.println(momo.name+momo.Eng+momo.KR+momo.Math);
+        System.out.println(momo.toString());
+
+        Score momo2 = momo;
+                System.out.println("정보 momo2 :"+momo2);
+        System.out.println(momo2.name+momo2.Eng+momo2.KR+momo2.Math);
+         momo.name = "momo";momo.KR=100;momo.Math=79;momo.Eng=8;
+         System.out.println("정보2"+momo);
+         System.out.println(momo.name+momo.Eng+momo.KR+momo.Math);
+         //문자열 : 불변 객체(immutable Object) - 특정 문자열이 메모리에 저장, 만약 주소가 100이면 해당 주소에는 다른 문자열로 바꿔 저장할 수 없다.
+         //Object 부모클래스의 메소드 : clone(),hashcode(),equals(),toString()
+         //toString()은 오버라이드 (재정의) : @Override는 실행코드가 아니라 개발관련 주석
     }
 }
 class Score{
@@ -21,4 +31,9 @@ class Score{
     int KR;
     int Math;
     int Eng;
+
+    @Override
+    public String toString(){
+        return "-ToString()-"+name+Eng+KR+Math;
+    }
 }
---
+++ PARTA/src/day02/StringForTest.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class StringForTest {
+    public static void main(String[] args) {
+        String test = "테스트";
+        for (int i = 0; i > 5; i++) {
+            test = test + " " + i;
+            System.out.println(test);
+        }
+    }
+}
