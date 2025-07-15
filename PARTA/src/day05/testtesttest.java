379f885d6ca9d83a03d3972ebe7181fc28383f78
GAMGO <gamgo445@gmail.com>
2025-07-15 18:00:40 +0900

    update 15_1

---
+++ PARTA/src/day05/testtesttest.java
@@ -0,0 +1,32 @@
+package day05;
+
+public class testtesttest {
+    public static void main(String[] args) {
+        // 1. 다음 코드의 출력 결과는? 3번
+        /*
+         * int[] arr = { 10, 20, 30, 40, 50 };
+         * System.out.println(arr.length);
+         * arr[2] = 100;
+         * System.out.println(arr[2]);
+         * //예상 5,100
+         */
+        // 2. 다음 중 객체 배열에 대한 설명으로 올바른 것은? 2번
+        // 3. 다음 코드에서 발생하는 예외는? 2번
+        // 4. 다음 try-catch 구문에서 출력 결과는? 2번
+        /*
+         * try {
+         * int result = 10 / 0;
+         * System.out.println("계산 완료");
+         * } catch (ArithmeticException e) {
+         * System.out.println("연산 오류");
+         * } finally {
+         * System.out.println("마무리");
+         * }
+         * System.out.println("프로그램 종료");
+         */
+        // 5. 다음 코드의 빈칸을 채우시오. 1:length 2:null
+        // 6. 다음 2차원 배열 코드의 빈칸을 채우시오. 1:length(3) 2:length(4)
+        // 7. 다음 예외 처리 코드의 빈칸을 채우시오. 1:throws 2:throw
+        // 8. 다음 배열 복사 코드의 빈칸을 채우시오. 1:length 2:length-1 3:clone || arraycopy 4:length-1
+    }
+}
